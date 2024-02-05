3cd4cd0e8c45480632431818a6afa47b1af4ab0a
Oxonantho <wendling.anthony@gmail.com>
2024-02-05 13:11:04 +0100

    second commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+// App.jsx
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Mon application React</h1>
+            <p>Bienvenue sur mon site!</p>
+        </div>
+    );
+}
